3dcb8ef35dd2f2e62e652a4a3942216b32c9fd93
Jorge A Lopez <jlobeto@gmail.com>
2020-07-16 22:12:52 -0500

    casos borde follow a user

--- src/main/java/com/jlopez/domain/User.java
+++ src/main/java/com/jlopez/domain/User.java
@@ -1,9 +1,6 @@
 package com.jlopez.domain;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class User {
     private String username;
@@ -18,14 +15,37 @@ public class User {
     }
 
     public void follow(User followee) {
+        if (this.equals(followee)) throw new CannotFollowYourself();
+        if (this.isFollowing(followee.getUsername())) throw new UserAlreadyFollowed();
+
         followees.put(followee.getUsername(),followee);
+    }
+
+    private boolean isFollowing(String username) {
+        return followees.containsKey(username);
     }
 
     public List<User> getFollowees() {
         return new ArrayList<>(followees.values());
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(username, user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username);
+    }
+
+    public class CannotFollowYourself extends RuntimeException {
+    }
 
 
-
+    public class UserAlreadyFollowed extends RuntimeException {
+    }
 }
--- src/test/java/com/unit/FollowAUserShould.java
+++ src/test/java/com/unit/FollowAUserShould.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -24,14 +25,67 @@ public class FollowAUserShould {
     private static final String ANY_USERNAME = "anyUsername";
     private static final String USERNAME_TO_FOLLOW = "otherUsername";
 
+    FollowAUser followAUser;
+
     @Test
     public void a_user_can_follow_other_user() {
-        FollowAUser followAUser = new FollowAUser(users);
+        given_a_followUser_action();
+        and_given_two_different_users_when_get_users_from_the_repository();
+
+        when_action_follow_action_is_executed_with(ANY_USERNAME, USERNAME_TO_FOLLOW);
+
+        then_the_first_user_follow_the_other_user();
+    }
+
+    @Test
+    public void an_user_cannot_follows_himself() {
+        given_a_followUser_action();
+        and_given_the_same_user_when_get_users_from_the_repository();
+
+        assertThatThrownBy(() -> when_action_follow_action_is_executed_with(ANY_USERNAME, ANY_USERNAME))
+                .isInstanceOf(User.CannotFollowYourself.class);
+    }
+
+    @Test
+    public void an_user_cannot_follow_an_already_followed_user() {
+        given_a_followUser_action();
+        and_given_a_any_user_that_follow_another_user();
+
+        assertThatThrownBy(() -> when_action_follow_action_is_executed_with(ANY_USERNAME, USERNAME_TO_FOLLOW))
+                .isInstanceOf(User.UserAlreadyFollowed.class);
+    }
+
+
+    private void given_a_followUser_action() {
+        followAUser = new FollowAUser(users);
+
+    }
+
+    private void and_given_two_different_users_when_get_users_from_the_repository() {
         when(users.get(ANY_USERNAME)).thenReturn(new User(ANY_USERNAME));
         when(users.get(USERNAME_TO_FOLLOW)).thenReturn(new User(USERNAME_TO_FOLLOW));
+    }
 
-        followAUser.execute(ANY_USERNAME, USERNAME_TO_FOLLOW);
+    private void and_given_the_same_user_when_get_users_from_the_repository() {
+        when(users.get(ANY_USERNAME)).thenReturn(new User(ANY_USERNAME));
+    }
 
+    private void and_given_a_any_user_that_follow_another_user() {
+        User anyUser = new User(ANY_USERNAME);
+        User userToFollow = new User(USERNAME_TO_FOLLOW);
+        anyUser.follow(userToFollow);
+
+        when(users.get(ANY_USERNAME)).thenReturn(anyUser);
+        when(users.get(USERNAME_TO_FOLLOW)).thenReturn(userToFollow);
+
+    }
+
+    private void when_action_follow_action_is_executed_with(String anyUsername, String usernameToFollow) {
+        followAUser.execute(anyUsername, usernameToFollow);
+    }
+
+
+    private void then_the_first_user_follow_the_other_user() {
         verify(users,times(1)).save(argument.capture());
         List<User> followees = argument.getValue().getFollowees();
         assertThat(followees.size()).isEqualTo(1);
