5d59ddfabe707b44185388f4eaca89268bb4d60e
Jorge A Lopez <jlobeto@gmail.com>
2020-07-16 22:26:48 -0500

    refactor test

--- src/test/java/com/unit/PostingShould.java
+++ src/test/java/com/unit/PostingShould.java
@@ -3,6 +3,7 @@ package com.unit;
 import com.jlopez.actions.MakeAPost;
 import com.jlopez.domain.Post;
 import com.jlopez.domain.Posts;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
@@ -27,15 +28,27 @@ public class PostingShould {
 
     private MakeAPost makeAPost;
 
+
     @Test
     public void a_user_can_publish_messages_to_a_personal_timeline() {
+        given_a_makeAPost_action();
+
+        when_the_action_is_executed_with(ANY_USERNAME, ANY_MESSAGE);
+
+        then_the_post_is_saved();
+    }
+
+    private void given_a_makeAPost_action() {
         makeAPost = new MakeAPost(posts);
+    }
 
-        makeAPost.execute(ANY_USERNAME, ANY_MESSAGE);
+    private void when_the_action_is_executed_with(String username, String message) {
+        makeAPost.execute(username, message);
+    }
 
+    void then_the_post_is_saved() {
         verify(posts, times(1)).addPost(eq(ANY_USERNAME), argument.capture());
         assertThat(argument.getValue().getMessage()).isEqualTo(ANY_MESSAGE);
-
     }
 
 }
